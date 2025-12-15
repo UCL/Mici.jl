6316b5d949ab1ac76cf84cbd6308dc742a3c26b2
Callum Lau <callum_lau@hotmail.com>
2025-12-15 22:11:30 +0000

    .

--- test/test_abstractmcmc.jl
+++ test/test_abstractmcmc.jl
@@ -15,7 +15,7 @@ using Mici.Mici: MetropolisHMCSampler, MetropolisTransition, LeapfrogIntegrator,
     model = EuclideanSystem(neg_log_dens, grad_neg_log_dens, metric)
     sampler = MetropolisHMCSampler(LeapfrogIntegrator(0.2, 10), MetropolisTransition())
 
-    samples = sample(rng, model, sampler, nsamples, chain_type=Any, progress=false)
+    @time samples = sample(rng, model, sampler, nsamples, chain_type=Any, progress=false)
 
     @test norm(mean(samples, dims=1)[1] - μ) < 0.3
 
