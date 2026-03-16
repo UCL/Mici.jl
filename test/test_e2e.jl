c8d0859f78ea56d5a277a17bf8ae47df6a480a05
Matt Graham <matthew.m.graham@gmail.com>
2026-03-16 16:30:09 +0000

    Adjust test tolerances and compare covariance rather than second moment

--- test/test_e2e.jl
+++ test/test_e2e.jl
@@ -7,15 +7,17 @@ using Mici.Mici: EuclideanHMC
     ℓ = 𝒩()
     model = LogDensityModel(ℓ)
     rng = StableRNG(1234)
-    C1 = 25
-    C2 = 40
+    C1 = 2.
+    C2 = 5.
 
-    sampler = EuclideanHMC(0.3)
+    sampler = EuclideanHMC(1.5, 3.0)
+
+    initial_q = randn(rng, 2)
 
     for n_samples in (1000, 10000, 100000)
-        samples = sample(rng, model, sampler, n_samples; initial_ϵ=0.05, progress=false)
+        samples = sample(rng, model, sampler, n_samples; initial_q, initial_ϵ=0.25, progress=false)
         q = samples.traces.q
         @test norm(mean(q, dims=2) - ℓ.μ) < C1 / sqrt(n_samples)
-        @test norm(q * q' / size(q, 2) - (ℓ.Σ + ℓ.μ * ℓ.μ')) < C2 / sqrt(n_samples)
+        @test norm(cov(q, dims=2) - ℓ.Σ) < C2 / sqrt(n_samples)
     end
 end
