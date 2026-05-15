7b9c6202b66aa2055cd30a7df3f9c3f41034babd
raahweng <raahweng28@gmail.com>
2026-05-15 12:19:32 +0100

    WIP. Get some plumbing inline to have different integrators.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,5 @@ using Test
 
 @testset "Mici.jl" begin
     include("test_e2e.jl")
-    include("test_end_to_end.jl")
-    include("gni_integration_tests.jl")
+    # include("gni_integration_tests.jl")
 end
--- test/test_e2e.jl
+++ test/test_e2e.jl
@@ -1,8 +1,28 @@
 include("dependencies_for_runtests.jl")
 
-using Mici.Mici: EuclideanHMC
+using Mici.Mici: EuclideanHMC, EuclideanHMC1
 
-@testset "Abstract MCMC e2e" begin
+# @testset "Abstract MCMC e2e" begin
+
+#     ℓ = 𝒩()
+#     model = LogDensityModel(ℓ)
+#     rng = StableRNG(1234)
+#     C1 = 2.
+#     C2 = 5.
+
+#     sampler = EuclideanHMC(1.5, 3.0)
+
+#     initial_q = randn(rng, 2)
+
+#     for n_samples in (1000, 10000, 100000)
+#         samples = sample(rng, model, sampler, n_samples; initial_q, initial_ϵ=0.25, progress=false)
+#         q = samples.traces.q
+#         @test norm(mean(q, dims=2) - ℓ.μ) < C1 / sqrt(n_samples)
+#         @test norm(cov(q, dims=2) - ℓ.Σ) < C2 / sqrt(n_samples)
+#     end
+# end
+
+@testset "GNI Abstract MCMC e2e" begin
 
     ℓ = 𝒩()
     model = LogDensityModel(ℓ)
@@ -10,7 +30,7 @@ using Mici.Mici: EuclideanHMC
     C1 = 2.
     C2 = 5.
 
-    sampler = EuclideanHMC(1.5, 3.0)
+    sampler = EuclideanHMC1(1.5)
 
     initial_q = randn(rng, 2)
 
