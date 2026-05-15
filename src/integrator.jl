f24aa03fd238a9cb5d3bc9482b0ec0f7e990e01b
Matt Graham <matthew.m.graham@gmail.com>
2026-05-15 15:51:23 +0100

    Adding ability to alter integration direction

--- src/integrator.jl
+++ src/integrator.jl
@@ -2,8 +2,8 @@ struct LeapfrogIntegrator{T} <: AbstractIntegrator
     ϵ::T
 end
 
-function step!(z::PhasePoint, integrator::LeapfrogIntegrator, system::AbstractTractableFlowSystem; dir=1)
-    Φ₁!(z, system, dir*integrator.ϵ / 2)
-    Φ₂!(z, system, dir*integrator.ϵ)
-    Φ₁!(z, system, dir*integrator.ϵ / 2)
+function step!(z::PhasePoint, integrator::LeapfrogIntegrator, system::AbstractTractableFlowSystem; direction=1)
+    Φ₁!(z, system, direction*integrator.ϵ / 2)
+    Φ₂!(z, system, direction*integrator.ϵ)
+    Φ₁!(z, system, direction*integrator.ϵ / 2)
 end
